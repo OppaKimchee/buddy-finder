0b80c759ac8617767002822bcf890f8c3aa8d6bd
tinachungshi <tinachungshi@gmail.com>
2017-05-16 12:09:04 -0700

    Fix submission form and categorize lost

--- models/pet.js
+++ models/pet.js
@@ -3,16 +3,16 @@ const Schema = mongoose.Schema;
 
 const petSchema = new Schema({
   category: {type: String, required: true, enum: ['Lost', 'Found']},
-  name: {type: String, required: true},
   type: {type: String, required: true, enum: ['Cat', 'Dog']},
-  breed: {type: String, required: true},
-  sex: {type: String, required: true, enum: ['Female', 'Male']},
-  color: {type: String, required: true},
-  size: {type: String, required: true, enum: ['Small', 'Medium', 'Large']},
+  sex: {type: String, enum: ['Female', 'Male', 'N/A']},
+  size: {type: String, enum: ['Small', 'Medium', 'Large', 'N/A']},
+  name: String,
+  breed: String,
+  color: String,
   age: Number,
   location: {type: String, required: true},
-  picture: {type: String, required: true},
-  description: {type: String, required: true}
+  description: String,
+  picture: String
 },
 {
   timestamp:true
--- routes/pets.js
+++ routes/pets.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pets = require('../controllers/pets');
 
-
 // start from /
 
 // map
