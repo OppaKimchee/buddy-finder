6dc631ce1aa5ed570bc8e70da8606e99253b733d
tinachungshi <tinachungshi@gmail.com>
2017-05-18 17:37:03 -0700

    About page, Pet and User schema updated

--- models/user.js
+++ models/user.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  category: String,
-  name: {type: String, required: true},
-  email: {type: String, required: true},
-  phone: String,
-  area: String,
+  name: String,
+  email: String,
   facebookId: String,
-  status: {type: String, enum: ['Lost', 'Found']},
   pets: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pet'}]
 });
 
